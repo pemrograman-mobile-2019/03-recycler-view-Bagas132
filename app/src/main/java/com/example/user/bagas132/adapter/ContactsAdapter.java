a80966bf8aaf02ece8409b427f8be21afa7d2339
Bagas132 <ekamahendrabagaskara@gmail.com>
2019-02-21 12:30:14 +0700

    Recycle View Praktikum

--- app/src/main/java/com/example/user/bagas132/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/user/bagas132/adapter/ContactsAdapter.java
@@ -1,11 +1,33 @@
 package com.example.user.bagas132.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by User on 2/21/2019.
  */
 
-public class ContactsAdapter extends RecyclerView.Adapter {
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
 
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
