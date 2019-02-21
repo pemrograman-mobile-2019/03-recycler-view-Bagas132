7f34fee687093a14f5cf8774f37f47e15aedfc1e
Bagas132 <ekamahendrabagaskara@gmail.com>
2019-02-21 12:30:54 +0700

    Recycle View Praktikum

--- app/src/main/java/com/example/user/bagas132/MainActivity.java
+++ app/src/main/java/com/example/user/bagas132/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.user.bagas132;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.user.bagas132.adapter.ContactsAdapter;
+import com.example.user.bagas132.models.Contact;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rv = findViewById(R.id.rvContact);
+
+        contacts = Contact.createContactsList(20);
+
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
     }
+
+
 }
--- app/src/main/java/com/example/user/bagas132/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/user/bagas132/adapter/ContactsAdapter.java
@@ -1,8 +1,18 @@
 package com.example.user.bagas132.adapter;
 
+import android.content.Context;
+import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.user.bagas132.R;
+import com.example.user.bagas132.models.Contact;
+
+import java.util.List;
 
 /**
  * Created by User on 2/21/2019.
@@ -10,24 +20,52 @@ import android.view.ViewGroup;
 
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    private List<Contact> mContacts;
+
+    public ContactsAdapter(List<Contact> contacts) {
+        this.mContacts = contacts;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
+
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        Contact contact = mContacts.get(position);
+
+        // Set item views based on your views and data model
+
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(contact.getName());
+        Button button = viewHolder.messageButton;
+        button.setText(contact.isOnline() ? "Message" : "Offline");
+        button.setEnabled(contact.isOnline());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mContacts.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTextView;
+        private Button messageButton;
         public ViewHolder(View itemView) {
             super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            messageButton = (Button) itemView.findViewById(R.id.message_button);
         }
     }
+
 }
